76bc3ad1c4b221559fb2d23cf7462bff9c3e2c95
Samuel-OR <samueloliveira0014@gmail.com>
2019-10-17 23:28:16 -0300

    feat: Url CRUD

--- amigos_da_comunidade/edicao/urls.py
+++ amigos_da_comunidade/edicao/urls.py
@@ -9,17 +9,21 @@ urlpatterns = [
 	path('editar/<int:pk>/', EdicaoUpdate.as_view(), name='edicao_update',),
 	path('deletar/<int:pk>/', edicao_delete, name='edicao_delete',),
 
+
 	# path('professor/adicionar/', ProfessorCreate.as_view(), name='professor_create',),
 	# path('professor/listar/', ProfessorList.as_view(), name='professor_list',),
 	# path('professor/editar/<int:pk>/', ProfessorUpdate.as_view(), name='professor_update',),
 	# path('professor/deletar/<int:pk>/', professor_delete, name='professor_delete',),
 
+
 	# path('nucelo-autocomplete/',NucleoAutocomplete.as_view(), name='nucleo_autocomplete',),
 	# path('instituicao-autocomplete/',InstituicaoAutocomplete.as_view(), name='instituicao_autocomplete',),
+
 
 	# path('aluno/adicionar/',AlunoCreate.as_view(), name='aluno_create',),
 	# path('aluno/listar/', AlunoList.as_view(), name='aluno_list',),
 	# path('aluno/editar/<int:pk>/', AlunoUpdate.as_view(), name='aluno_update',),
 	# path('aluno/deletar/<int:pk>/', aluno_delete, name='aluno_delete',), 
+
 	
 ]
